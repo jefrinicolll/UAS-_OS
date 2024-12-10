9e89d56224b6e6ddcd092f10a2bc7595a2349dea
jefrinicolll <jefripakonglean@gmail.com>
2024-12-10 07:23:04 +0000

    first commit

---
+++ konversi.c
@@ -0,0 +1,21 @@
+void dolar () {
+float rupiah,dolar;
+printf("masukan jumlah rupiah :");
+scanf("%f",&rupiah);
+dolar=rupiah /15943;
+printf ("\t nilai konversi rupiah ke dolar adalah : $ %f\n",  dolar);
+
+}
+
+void rupiah ()
+{
+ float rupiah,dolar;
+ 
+ printf("masukan jumlah dolar : ");
+ scanf("%f",&dolar);
+ rupiah=dolar * 159443;
+ printf ("t\ nilai konversi rupiah ke dolar adalah :Rp %f\n",rupiah);
+ 
+ }
+ 
+ 
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "jef.h"
+
+int main ()
+{
+
+  printf("\t\t library sistem operasi\n");
+  printf("\n ============================ \n");
+  printf("welcome to converter money");
+  printf("\n ============================ \n");
+  printf("\n nilai dolar ke rupiah \n");
+  rupiah();
+  
+  printf("\n nilai rupiah ke dolar \n");
+  dolar();
+ } 
+ 
